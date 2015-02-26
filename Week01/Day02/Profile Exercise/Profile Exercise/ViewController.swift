a5bd8bcb86a42281b6bc5d0b6b19d4f583ad0892
kevinkhchan <kevinkhchan@mac.com>
2015-02-26 22:39:21 +1100

    Profile Exercise Start
    Establishing files for the Profile Exercise on Day02

---
+++ Week01/Day02/Profile Exercise/Profile Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile Exercise
+//
+//  Created by Kevin Chan on 26/02/2015.
+//  Copyright (c) 2015 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
