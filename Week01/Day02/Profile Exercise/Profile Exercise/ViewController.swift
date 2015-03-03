fdb8b19796d916239d94a00b399d8d4f6339ceb6
kevinkhchan <kevinkhchan@mac.com>
2015-03-04 00:56:37 +1100

    Basic Login Screen Created
    A basic login screen has been generated and a segue created from the
    login screen to the My Profile screen.

--- Week01/Day02/Profile Exercise/Profile Exercise/ViewController.swift
+++ Week01/Day02/Profile Exercise/Profile Exercise/ViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func unwindFromSegue(segue: UIStoryboardSegue) {
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
