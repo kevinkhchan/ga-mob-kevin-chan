f18b33d56fc4a4bc9ed76f04983eb7649c366c79
kevinkhchan <kevinkhchan@mac.com>
2015-03-05 18:00:19 +1100

    Amended Login Screen
    Made amendments from name input to email input.

---
+++ Week01/Day02/Profile Exercise/Profile Exercise/ViewController/KEVMyProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  KEVMyProfileViewController.swift
+//  Profile Exercise
+//
+//  Created by Kevin Chan on 5/03/2015.
+//  Copyright (c) 2015 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class KEVMyProfileViewController: UIViewController {
+    
+    @IBAction func unwindToMyProfileViewController(segue: UIStoryboardSegue) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
