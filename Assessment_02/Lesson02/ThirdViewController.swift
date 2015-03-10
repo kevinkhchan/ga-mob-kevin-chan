e9717d1bb1a66b3c354cdf1f0ea710ef47d819d8
kevinkhchan <kevinkhchan@mac.com>
2015-03-10 15:51:26 +1100

    VC3 Completed.
    Third View Controller has been completed.

--- Assessment_02/Lesson02/ThirdViewController.swift
+++ Assessment_02/Lesson02/ThirdViewController.swift
@@ -13,4 +13,32 @@ class ThirdViewController: UIViewController {
     TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
 
 */
+    
+    @IBOutlet weak var thirdViewNumber: UITextField!
+    @IBOutlet weak var thirdViewLabel: UILabel!
+    
+    @IBAction func thirdViewButton(sender: AnyObject) {
+        if thirdViewNumber.text != "" {
+            
+            var numberChecked: Int = thirdViewNumber.text.toInt()!
+
+            if numberChecked%2 == 0 {
+                thirdViewLabel.text = "The number \(numberChecked) is even."
+            } else {
+                thirdViewLabel.text = "The number \(numberChecked) is not even."
+            }
+            
+            thirdViewNumber.text = ""
+            self.view.endEditing(true)
+            
+        } else {
+            thirdViewLabel.text = "Please enter a number."
+            thirdViewNumber.becomeFirstResponder()
+        }
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+
 }
