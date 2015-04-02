a9ce7bc60b8573bdb9a4c464fc9e571f95d1edd0
kevinkhchan <kevinkhchan@mac.com>
2015-04-02 15:32:00 +1100

    Connected the numeral and decimal buttons
    The numeral buttons have been connected to an IBAction and the decimal
    button has been to connected to an IBAction.

--- Assessment_04/kCalc/kCalc/ViewController.swift
+++ Assessment_04/kCalc/kCalc/ViewController.swift
@@ -9,7 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Declare some variables
+    
+    var calcLabelValue: String! = "0" // start with no value in the label as it is 0
+    var hasDecimal: Bool = false // has the decimal been pressed - start with no
+    
+    // Add the label as an IBOutlet
 
+    @IBOutlet weak var calcLabel: UILabel!
+    
+    // Respond to the tapping of numbers
+    /*
+        Connect all the numeral buttons to an IBAction.
+        Check if the exisiting number is 0. If so replace with the number pressed.
+        If the exisiting number is not 0 then append the number pressed to the end of the string.
+    */
+    
+    
+    @IBAction func tapNumbers(sender: UIButton) {
+        
+        let tappedNumber : String! = sender.titleLabel!.text
+
+        if calcLabel.text == "0" {
+            calcLabelValue = tappedNumber
+        } else {
+            calcLabelValue = calcLabelValue.stringByAppendingString(tappedNumber)
+        }
+        calcLabel.text = calcLabelValue
+
+    }
+    
+    // Respond to the tapping of the decimal
+    
+    /*
+        Connect the decimal button to an IBAction.
+        Add the decimal point if there is no existing decimal point.
+    */
+    
+    @IBAction func tapDecimal(sender: AnyObject) {
+        if hasDecimal == false {
+            calcLabelValue = calcLabelValue.stringByAppendingString(".")
+            calcLabel.text = calcLabelValue
+            hasDecimal = true
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +65,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
